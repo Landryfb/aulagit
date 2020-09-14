9bc543980beef8325f81548b5ffbf6bb1af76e8c
LANDRY FERNANDES BARATA <003925419@itaud.des.ihf>
2020-09-14 12:03:16 -0300

    Adicionado Novo

--- Exemplo01.java
+++ Exemplo01.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 /**
  * Exemplo01
  */
 public class Exemplo01 {
-    
 
+public static void main(String[] args) {
+    Scanner entradaScanner = Scanner(System.in);
+
+
+    
+}
     
 }
